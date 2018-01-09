0ed92b8a6762f7bc3dc57007ed7d116cd74d1880
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-01-10 00:37:34 +0300

    Создание Maven-проекта

---
+++ src/main/java/ru/ak/sftpclient/SftpClient.java
@@ -0,0 +1,14 @@
+package ru.ak.sftpclient;
+
+import javax.jws.WebService;
+
+
+/**
+ * Web-сервис для реализации интерфейса взаимодействия с SFTP-сервером
+ * @author akakushin
+ */
+@WebService(name = "SftpClient", serviceName = "SftpClient", portName = "SftpClientPort")
+public class SftpClient {
+
+
+}
